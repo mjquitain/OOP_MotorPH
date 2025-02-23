87a557e2edb18d7b4e550f723220c205040fac8f
mjquitain <145683592+mjquitain@users.noreply.github.com>
2025-02-23 18:25:53 +0800

    feat: initialized backend

---
+++ backend/src/main/java/com/example/motorph/MotorphApplication.java
@@ -0,0 +1,13 @@
+package com.example.motorph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorphApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/motorph/MotorphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
